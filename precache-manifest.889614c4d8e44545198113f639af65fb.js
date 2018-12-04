ba088010be8fe3570f13ab00b36c77ae0ccba4ff
Steven La <stevenla@g.ucla.edu>
2018-12-04 14:18:34 -0800

    Updates

---
+++ precache-manifest.889614c4d8e44545198113f639af65fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a24ea7259380d29bc41727123c8d50ac",
+    "url": "/color/static/media/CrayonHandRegular2016Demo.a24ea725.ttf"
+  },
+  {
+    "revision": "fa073fba6fa3024b544c5f9741e2b50b",
+    "url": "/color/static/media/AhoyAmigo-DEMO.fa073fba.ttf"
+  },
+  {
+    "revision": "4a6969dd3fb0e1f3d466",
+    "url": "/color/static/js/runtime~main.4a6969dd.js"
+  },
+  {
+    "revision": "6063b6ba1dbfa1f95b28",
+    "url": "/color/static/js/main.6063b6ba.chunk.js"
+  },
+  {
+    "revision": "5bcf64ff254e595838bd",
+    "url": "/color/static/js/1.5bcf64ff.chunk.js"
+  },
+  {
+    "revision": "6063b6ba1dbfa1f95b28",
+    "url": "/color/static/css/main.327cf2ef.chunk.css"
+  },
+  {
+    "revision": "ddec838bce98180ff9e2275e59952829",
+    "url": "/color/index.html"
+  }
+];
